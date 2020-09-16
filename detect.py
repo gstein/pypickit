ed890a466d82bb8a2df84426466b1ba315b54b92
Greg Stein <gstein@gmail.com>
2020-09-16 09:54:48 -0500

    move PICkit class into util.py

--- detect.py
+++ detect.py
@@ -5,63 +5,7 @@
 
 import sys
 
-import usb.core  # python3 -m pip install pyusb
-
 import util
-
-
-### are there other boards out there? using other IDs?
-ID_VENDOR = 0x04d8
-ID_PRODUCT = 0x0033
-
-# Use the vendor-specific configuration, and endpoints
-CONFIG_VENDOR = 2
-ENDPOINT_IN = 0x81
-ENDPOINT_OUT = 0x01
-
-
-class PICkit:
-    def __init__(self):
-        ### multiple programmers on the bus? f'it. assume one.
-        self.device = usb.core.find(idVendor=ID_VENDOR,
-                                    idProduct=ID_PRODUCT)
-        #print('FOUND:', self.device)
-
-        ### Ignore the various race/reset issues with configurations
-        ### http://libusb.sourceforge.net/api-1.0/libusb_caveats.html#configsel
-        self.device.set_configuration(CONFIG_VENDOR)
-
-        interface = self.device.get_active_configuration()[0,0]
-        endpoints = interface.endpoints()
-
-        ### for now, don't worry about !=2 endpoints
-        assert len(endpoints) == 2
-        if endpoints[0].bEndpointAddress == ENDPOINT_IN:
-            self.ep_in = endpoints[0]
-            self.ep_out = endpoints[1]
-        else:
-            self.ep_in = endpoints[1]
-            self.ep_out = endpoints[0]
-        assert self.ep_in.bEndpointAddress == ENDPOINT_IN
-        assert self.ep_out.bEndpointAddress == ENDPOINT_OUT
-
-        ### claim the interface?
-
-        self.write(util.FWCMD_FIRMWARE_VERSION)
-        result = self.read(8)
-        ### check for bootloader mode
-        print('VERSION:', tuple(result)[:3])
-
-    def write(self, cmd, *values):
-        v = bytes((cmd,) + values)  # ensures all values in [0,255]
-        print('WRITE:', v)
-        return self.ep_out.write(v)
-
-    def read(self, amt):
-        ### ignore AMT. read frames are always 64 bytes.
-        ### what to do?
-        assert amt <= 64
-        return bytes(self.ep_in.read(64))[:amt]
 
 
 def set_vdd(pk, voltage, threshold):
@@ -119,5 +63,5 @@ def detect(pk):
 
 
 if __name__ == '__main__':
-    pk = PICkit()
+    pk = util.PICkit()
     #set_vdd(pk, 3.0, 0.85)
